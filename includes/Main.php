624d3a0079f4947f8c8731f8aee524c9820a2d01
Lukas Niebler <nieblerlukas@gmail.com>
2021-12-03 14:50:29 +0100

    Orientieren an FAU Fehlermeldungen Plugin und Übernahme von Code

---
+++ includes/Main.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace RRZE\vRevision;
+
+defined('ABSPATH') || exit;
+
+
+use RRZE\vRevision\Shortcode;
+
+/**
+ * Hauptklasse (Main)
+ */
+
+class Main {
+    /**
+     * Der vollständige Pfad- und Dateiname der Plugin-Datei.
+     * @var string
+     */
+    protected $pluginFile;
+
+    /**
+     * Variablen Werte zuweisen.
+     * @param string $pluginFile Pfad- und Dateiname der Plugin-Datei
+     */
+    public function __construct($pluginFile){
+        $this->pluginFile = $pluginFile;
+    }
+
+    /**
+     * Es wird ausgeführt, sobald die Klasse instanziiert wird.
+     */
+    public function onLoaded() {
+        
+    }
+    }
