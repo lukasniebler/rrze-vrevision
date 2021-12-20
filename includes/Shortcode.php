3f8c42a073bd4467aea5dbe38cf002e9a378e044
Lukas Niebler <nieblerlukas@gmail.com>
2021-12-20 12:29:16 +0100

    Rückgängig machen
    Es hat nicht geklappt die Bilder dynamisch auszugeben. Zwar wurden die Bilder immer dann dynamisch ausgegeben, wenn man sie direkt in das Test-Template eingebunden hat. Sobald allerdings die Variable für die Bilder erneut aufgerufen wurde, gab es keinen Output mehr für die URL variable. Die Funktionen zur Generierung der URL gaben einen korrekten path-String aus. Lediglich der Parser schien mit der Eingabe nicht zurecht zu kommen. Bzw. die URL aus unbekannten Gründen zu ignorieren

--- includes/Shortcode.php
+++ includes/Shortcode.php
@@ -72,15 +72,6 @@ class Shortcode
         $this->data->imgcenter = $this->getTemplateParts('img-center');
         $this->data->list = $this->getTemplateParts('list');
         $this->data->code = $this->getTemplateParts('code');
-        $this->data->test = $this->getImgNames("Workflow1024");
-        $this->data->imagunA = $this->getImgpath('1024','Workflow','Original');
-       
-        //$this->data->imagun1024 = $this->getImgpath('1024','Workflow','Original');
-        $this->data->imagunB = $this->getImgpath('300','Workflow','Original');
-        $this->data->imagunC = $this->getImgpath('150','Workflow','Original');
-        $this->data->imagunO = $this->getImgpath('original','Workflow','Original');
-
-        //$this->data->img1024 = $this->
 
         /**
          * Following Arrays are getting 10 stacks of their elements to create individual content-placeholders.
@@ -141,55 +132,10 @@ class Shortcode
         }
         $content = Template::getContent($template, $this->data);
         if (!empty($content)) {
-            var_dump($content);
             return $content;
         } else {
             $msg = "<!-- No Entry found for Error " . $type . "-->";
             return $msg;
         }
     }
-    /**
-     * Returns an image-Filename out of the Workflow1024 Folder for reference.
-     *
-     * @return string
-     */
-    public function getImgNames($targetDir){
-        $dir = plugin_dir_path( __DIR__ );
-        $path = $dir."assets/img/".$targetDir;
-        $files = array_diff(scandir($path), array('..', '.'));
-        $randomNumber = rand(2, count($files));
-        
-        return $files[$randomNumber] ?? null;
-    }
-
-    /**
-     * Returns a image path by setting the following parameters
-     *
-     * @param string $res Resolution | 1024, 300, 150 or original
-     * @param string $foldername | prefix of Folder with different resolutions. f.e. imagefolder300 imagefolder1024 etc.
-     * @param string $ref | Foldername of original-files
-     * @return string returns a imagepath
-     */
-    public function getImgpath($res, $foldername, $ref){
-        $resolution = '';
-        switch($res) {
-            case '1024':
-                $resolution = $foldername.'1024';
-                break;
-            case '300':
-                $resolution = $foldername.'300';
-                break;
-            case '150':
-                $resolution = $foldername.'150';
-                break;
-            case 'original':
-                $resolution = $ref;
-                break;
-        }
-        $urlpartial = 'assets/img/'.$resolution.'/'.$this->getImgNames($resolution);
-        $dir = untrailingslashit(plugins_url($urlpartial, $this->pluginFile));
-        var_dump($dir);
-        return $dir;
-    }
-    
 }
