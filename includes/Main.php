4c2b22aec69a787b20d06fdd780b7ca46c17521e
Lukas Niebler <nieblerlukas@gmail.com>
2022-02-01 20:18:43 +0100

    🪄 | Vorbereitung der Einstellungsseite
    Die Einstellungsseite soll es später schneller ermöglichen, die Fakultätsfarben und Bildinhalte zu wählen

--- includes/Main.php
+++ includes/Main.php
@@ -7,26 +7,46 @@ defined('ABSPATH') || exit;
 
 use RRZE\vRevision\Shortcode;
 
-/**
- * Hauptklasse (Main)
- */
-
 class Main
 {
     /**
-     * Der vollständige Pfad- und Dateiname der Plugin-Datei.
+     * Full path- and file name of plugin.
      * @var string
      */
     protected $pluginFile;
 
     /**
      * Variablen Werte zuweisen.
-     * @param string $pluginFile Pfad- und Dateiname der Plugin-Datei
+     * @param string $pluginFile Path and file name of plugin
      */
     public function __construct($pluginFile)
     {
         $this->pluginFile = $pluginFile;
+        // Hook into the admin menu
+        add_action('admin_menu', array( $this, 'create_plugin_settings_page'));
     }
+
+    public function create_plugin_settings_page() {
+        $page_title = 'RRZE vRevision Einstellungen';
+        $menu_title = 'RRZE vRevision';
+        $capability = 'manage_options';
+        $slug = 'vRevision_fields';
+        $callback = array ( $this, 'plugin_settings_page_content');
+        add_submenu_page( 'options-general.php', $page_title, $menu_title, $capability, $slug, $callback);
+    }
+
+    public function plugin_settings_page_content() { ?>
+        <div class="wrap">
+            <h2>Musterinhalt Defaultwerte</h2>
+            <form method="post" action="options.php">
+                <?php
+                    settings_fields( 'vRevision_fields' );
+                    do_settings_sections( 'vRevision_fields' );
+                    submit_button();
+                ?>
+            </form>
+        </div> <?php
+    }    
 
     /**
      * Es wird ausgeführt, sobald die Klasse instanziiert wird.
